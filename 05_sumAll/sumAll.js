262ee89334bbada16e2f784b4777ae42fc3668f0
Bdebrasi <bdebrasi@gmail.com>
2025-01-28 15:19:53 -0500

    05 exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(startNum,endNum) {
+    if (typeof startNum !== 'number' || typeof endNum !== 'number'){
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)){
+        return "ERROR";
+    }
+    let tmp = startNum;
+    startNum = Math.min(startNum,endNum);
+    endNum = Math.max(tmp,endNum);
+
+    if (startNum < 0){
+        return "ERROR";
+    }
+
+    let ans = 0;
+    for (let i = startNum; i < endNum + 1; i++){
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
