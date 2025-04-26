51ef63463cb262fab96f48aeca25ae1efcdff437
Bdebrasi <bdebrasi@gmail.com>
2025-04-25 20:03:54 -0400

    object exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const today = new Date();
+    const year = today.getFullYear();
+    return people.reduce((oldest, current) => {
+        const oldestAge = (oldest.yearOfDeath || year) - oldest.yearOfBirth;
+        const currentAge = (current.yearOfDeath || year) - current.yearOfBirth;
 
+        if(oldestAge >= currentAge){
+            return oldest;
+        } else {
+            return current;
+        }
+    });
 };
 
 // Do not edit below this line
